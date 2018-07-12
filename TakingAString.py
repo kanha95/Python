eacdf424d475bbcfcc2ff1af11030312f4069e60
Ruturaj Mohanty <kanha95@gmail.com>
2018-07-13 02:54:26 +0530

    Taking a string input and converting it to int

---
+++ TakingAString.py
@@ -0,0 +1,20 @@
+
+
+temp1 = map(int, raw_input().split())
+
+#or
+
+temp2 = [int(i) for i in raw_input().split()]
+
+
+print temp1[2]
+
+print temp2[1]
+
+#input
+# 3 2 4
+# 4 3 2 1
+
+#output
+#4
+#3
